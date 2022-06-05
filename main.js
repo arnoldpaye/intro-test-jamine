8d4b8b4cb78587542998d5f5d85ca2ddb59a3066
Arnold Paye <arnoldpayet@gmail.com>
2022-06-05 10:20:04 -0400

    challenge: your first test

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
-// Unit Testing: Our first test with it
+// Unit Testing: It Test Challenge
 
 /**
- * Test Suite
+ * 1. Write a unit test for lastName and middleName
+ * to test its default values.
+ * 2. Group all 3 unit test with an appropiate describe.
  */
+
+// Test Suite
 describe(`${Person.name} Class`, () => {
-  it('first name defaults to empty string', () => {
-    // arrange
-    const data = { firstName: null }
-
-    // action
-    const model = new Person(data);
-
-    // assert
-    expect(model.firstName).toBe('');
-  });
+  describe('default values', () => {
+    it('first name defaults to empty string', () => {
+      // arrange
+      const data = { firstName: null }
+  
+      // action
+      const model = new Person(data);
+  
+      // assert
+      expect(model.firstName).toBe('');
+    });
+  
+    it('last name defaults to empty string', () => {
+      // arrange
+      const data = { lastName: null }
+  
+      // action
+      const model = new Person(data);
+  
+      // assert
+      expect(model.lastName).toBe('');
+    });
+  
+    it('middle name defaults to empty string', () => {
+      // arrange
+      const data = { middleName: null }
+  
+      // action
+      const model = new Person(data);
+  
+      // assert
+      expect(model.middleName).toBe('');
+    });
+  })
 });
