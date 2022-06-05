101be1fcc246a2530bee291d202d670beeb5dc8c
Arnold Paye <arnoldpayet@gmail.com>
2022-06-05 10:10:10 -0400

    grouping with describe()

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-describe(' Modal Component', () => {
-  it('opens on click', () => {
-    // assert
-    expect(true).toBe(true);
-  });
+/**
+ * Unit Testing - Grouping with Describe Challenge
+ * 
+ * Is there a more reliable way to identify our grouping name?
+ */
+
+class Person {
+  firstName;
+  lastName;
+  middleName;
+
+  constructor(data = {}) {
+    this.firstName = data.firstName || '';
+    this.lastName = data.lastName || '';
+    this.middleName = data.middleName;
+  }
+}
+/**
+ * Test Suite
+ */
+describe(`${Person.name} Class`, () => {
+
 });
