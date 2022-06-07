00d34d7634015bb484d5dab1d364682d1bae74a8
Arnold Paye <arnoldpayet@gmail.com>
2022-06-07 08:29:13 -0400

    challenge: data setup with beforeEach

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// Unit Testing: It Test Challenge
+// Unit Testing: Setting Up Data with beforeEach Challenge
 
 /**
- * 1. Write a unit test for lastName and middleName
- * to test its default values.
- * 2. Group all 3 unit test with an appropiate describe.
+ * 1. Add a new describe for the fullName
+ * 2. Fully test the fullName get
+ * 3. Use a nested beforeEach
  */
 
 // Test Suite
@@ -12,26 +12,59 @@ describe(`${Person.name} Class`, () => {
 
   beforeEach(() => {
     model = new Person();
-    console.log('Before Each');
   });
 
   describe('default values', () => {
     it('first name defaults to empty string', () => {
-      console.log(1);
       // assert
       expect(model.firstName).toBe('');
     });
   
     it('last name defaults to empty string', () => {
-      console.log(2);
       // assert
       expect(model.lastName).toBe('');
     });
   
     it('middle name defaults to empty string', () => {
-      console.log(3);
       // assert
       expect(model.middleName).toBe('');
     });
-  })
+  });
+
+  describe('full name', () => {
+    beforeEach(() => {
+      model = new Person({
+        firstName: 'Dylan',
+        lastName: 'Israel'
+      });
+    });
+
+    it('middle initial when middle name is defined with first and last', () => {
+      // arrange
+      model.middleName = 'Christopher';
+
+      // act
+      const result = model.fullName;
+
+      // audit
+      const { firstName: fn, lastName: ln, middleName: mn } = model;
+
+      // assert
+      expect(result).toBe(`${fn} ${mn[0]}. ${ln}`);
+    });
+
+    it('when NO middle name returns just first and last', () => {
+      // arrange
+      model.middleName = '';
+
+      // act
+      const result = model.fullName;
+
+      // audit
+      const { firstName: fn, lastName: ln } = model;
+
+      // assert
+      expect(result).toBe(`${fn} ${ln}`);
+    });
+  });
 });
